20f0890357e8d0ea2161f5d2062008d73aa62b45
naclee01 <naclee01@gmail.com>
2019-11-08 23:14:17 -0500

    Initial push

---
+++ emails/begin_gpg.py
@@ -0,0 +1,29 @@
+import os, smtplib
+
+# set email and password for sending email
+email = 'sender@address.com'
+password = 'password'
+
+sent_from = email
+sent_to = 'recipient@address.com'
+subject = 'Raspberry Pi - GPG Begin'
+body = 'The GPG process has begun.'
+
+email_text = """\
+From: %s
+To: %s
+Subject: %s
+
+%s
+""" % (sent_from, sent_to, subject, body)
+
+try:
+	server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+	server.ehlo()
+	server.login(email, password)
+	server.sendmail(sent_from, sent_to, email_text)
+	server.close()
+
+	print ('Email sent!')
+except:
+	print ('Something went wrong...')
