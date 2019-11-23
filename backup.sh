7b5b00db8198ec79cbc6377d09dc13e6c1bc9f18
naclee01 <naclee01@gmail.com>
2019-11-22 23:01:52 -0500

    Update to use dates in filenames.

--- backup.sh
+++ backup.sh
@@ -2,26 +2,29 @@
 
 #Script to automate creating a backup from the shared samba server, encrypt the backup, and upload to AWS
 
+TAR=/path/to/$(date +%Y%m%d).tar
+GPG=/path/to/$(date +%Y%m%d).tar.gpg
+
 if [ -f ~/complete ] #Check to see if the tar process is complete. The tar process creates this file in the home directory when complete
 then
 
 	python3 path/to/begin_gpg.py #Email that the GPG encryption process has begun
 
-	gpg -r recipient@email.com -e ~/upload.tar #Encrypt the new tar
+	gpg -r recipient@email.com -e $TAR #Encrypt the new tar
 
 	python3 path/to/end_gpg.py #Email that the GPG encryption is complete
 
 	python3 path/to/begin_upload.py #Email that the upload to AWS has begun
 
-	aws s3 cp ~/upload.tar.gpg s3://bucket #Upload the encrypted tar to AWS
+	aws s3 cp $GPG s3://bucket #Upload the encrypted tar to AWS
 
 	python3 path/to/end_upload.py #Email that the upload has completed
 
 	python3 path/to/begin_delete.py #Email that the clean-up process has begun
 
-	rm ~/upload.tar #Remove the unencrypted tar
+	rm $TAR #Remove the unencrypted tar
 
-	rm ~/upload.tar.gpg #Remove the encrypted tar
+	rm $GPG #Remove the encrypted tar
 
 	rm ~/complete #Remove the flag file
 
--- tar.sh
+++ tar.sh
@@ -2,10 +2,12 @@
 
 #Script to automate creating a tar archive from the shared samba server
 
+$TAR=/path/to/$(date +%Y%m%d).tar
+
 python3 path/to/begin_tar.py #Email that the tar process has started
 
-tar -cf ~/upload.tar path/to/samba/share #Archive the samba directory to a tar file
+tar -cf $TAR path/to/samba/share #Archive the samba directory to a tar file
 
 python3 ~/end_tar.py #Email that the tar process has ended
 
-touch ~/complete
+touch ~/complete.txt
